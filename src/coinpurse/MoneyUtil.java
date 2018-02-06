a92c52c834af36693c33c0eb6ebcf150fb2d029f
Kornphon Noiprasert <konphon22@hotmail.com>
2018-02-06 22:43:54 +0700

    Finish fixed issues -LAB3-

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -5,15 +5,15 @@ import java.util.Comparator;
 import java.util.List;
 
 /**
- * MoneyUtil class use to sort the coins or banknotes, print list of values, separate the same
- * currency, test sort and comparedTo method.
+ * MoneyUtil class use to sort the coins or banknotes, print list of values,
+ * separate the same currency, test sort and comparedTo method.
  * 
  * @author Kornphon Noiprasert
  */
 public class MoneyUtil {
 
 	private static Comparator<Valuable> comp = new ValueComparator();
-	
+
 	/**
 	 * Configure and start the application.
 	 * 
@@ -24,7 +24,8 @@ public class MoneyUtil {
 	}
 
 	/**
-	 * Demonstrate that compareTo() method and sort by value of coins are correct including filterByCurrency.
+	 * Demonstrate that compareTo() method and sort by value of coins are correct
+	 * including filterByCurrency.
 	 */
 	public static void sortCoins() {
 		List<Coin> coins = new ArrayList<Coin>();
@@ -45,25 +46,30 @@ public class MoneyUtil {
 		coins.add(new Coin(-50, "Rupie"));
 		printCoins(coins);
 		System.out.println("===After Sort===");
-		java.util.Collections.sort(coins,comp);
+		java.util.Collections.sort(coins, comp);
 		printCoins(coins);
 		System.out.println("===Test compareTo method===");
 		// test compareTo method - 3 cases
 		// case 1. value a has order before value b. (result : -1).
-		// (0-Rupie).compareTo(0.50-Baht).
-		System.out.printf("(%.0f-%s).comparedTo(%.1f-%s).\n", coins.get(0).getValue(), coins.get(0).getCurrency(),
+		// (20-Baht).compareTo(50-Baht).
+		System.out.printf("(%.2f-%s).comparedTo(%.2f-%s).\n", coins.get(0).getValue(), coins.get(0).getCurrency(),
 				coins.get(1).getValue(), coins.get(1).getCurrency());
 		System.out.printf("returned : %d\n", coins.get(0).compareTo(coins.get(1)));
 		// case 2. value a has order after value b. (result : 1).
-		// (1-Baht).compareTo(0.50-Baht).
-		System.out.printf("(%.0f-%s).comparedTo(%.1f-%s).\n", coins.get(2).getValue(), coins.get(2).getCurrency(),
+		// (2-Baht).compareTo(1-Baht).
+		System.out.printf("(%.2f-%s).comparedTo(%.2f-%s).\n", coins.get(2).getValue(), coins.get(2).getCurrency(),
 				coins.get(1).getValue(), coins.get(1).getCurrency());
 		System.out.printf("returned : %d\n", coins.get(2).compareTo(coins.get(1)));
 		// case 3. value a and b have same order. (result : 0).
 		// (1-Rupie).compareTo(1-Rupie).
-		System.out.printf("(%.0f-%s).comparedTo(%.0f-%s).\n", coins.get(7).getValue(), coins.get(7).getCurrency(),
+		System.out.printf("(%.2f-%s).comparedTo(%.2f-%s).\n", coins.get(7).getValue(), coins.get(7).getCurrency(),
 				coins.get(8).getValue(), coins.get(8).getCurrency());
 		System.out.printf("returned : %d\n", coins.get(7).compareTo(coins.get(8)));
+		// case 4. value a and b by ValueComparator method Test
+		// (50-Baht).compareTo(1-Rupie).
+		System.out.printf("(%.2f-%s).comparedTo(%.2f-%s).\n", coins.get(5).getValue(), coins.get(5).getCurrency(),
+				coins.get(8).getValue(), coins.get(8).getCurrency());
+		System.out.printf("returned : %d\n", comp.compare(coins.get(5), coins.get(8)));
 		System.out.println("==================End Coins====================");
 		System.out.println("===Start test filter and sort===");
 		List<Valuable> values = new ArrayList<Valuable>();
@@ -85,11 +91,12 @@ public class MoneyUtil {
 			System.out.println(oneCoin);
 		}
 	}
-	
+
 	/**
 	 * Print a list of values.
 	 * 
-	 * @param values is a list of values with many currency values.
+	 * @param values
+	 *            is a list of values with many currency values.
 	 */
 	public static void printValues(List<Valuable> values) {
 		for (Valuable oneValue : values) {
@@ -107,25 +114,26 @@ public class MoneyUtil {
 	public static List<Valuable> filterByCurrency(List<Valuable> values, String currency) {
 		List<Valuable> toRemove = new ArrayList<Valuable>();
 		for (Valuable value : values) {
-		    if (!value.getCurrency().equals(currency)) {
-		        toRemove.add(value);
-		    }
+			if (!value.getCurrency().equals(currency)) {
+				toRemove.add(value);
+			}
 		}
 		List<Valuable> copy = new ArrayList<Valuable>();
 		copy.addAll(values);
 		for (Valuable value : toRemove) {
-		    copy.remove(value);
+			copy.remove(value);
 		}
 		return copy;
 	}
 
 	/**
-	 * Sort all values by using value of coin or bank to sort and print a list of values.
+	 * Sort all values by using value of coin or bank to sort and print a list of
+	 * values.
 	 * 
 	 * @param values is list of coins or banknotes with many currency values.
 	 */
 	public static void sortValues(List<Valuable> values) {
-		java.util.Collections.sort(values,comp);
+		java.util.Collections.sort(values, comp);
 		printValues(values);
 	}
 }
--- src/coinpurse/ValueComparator.java
+++ src/coinpurse/ValueComparator.java
@@ -10,24 +10,29 @@ import java.util.Comparator;
 public class ValueComparator implements Comparator<Valuable> {
 
 	/**
-	 * Compare two objects that implement Valuable.
-	 * First compare them by currency how many alphabets are more, so that "Baht" < "Dollar".
-	 * If both objects have the same currency, order them by value.
+	 * Compare two objects that implement Valuable. First compare them by currency
+	 * how many alphabets are more, so that "Baht" < "Dollar". If both objects have
+	 * the same currency, order them by value.
 	 * 
-	 * @param a is a value of coin or banknote that have a value and currency (or serialNumber).
-	 * @param b is a value of coin or banknote that have a value and currency (or serialNumber).
-	 * @return -1(negative integer) if a has order before b.
-	 *         0(zero) if a and b have the same order.
-	 *         1(positive integer) if a has order after b.
+	 * @param a is a value of coin or banknote that have a value and currency (or
+	 *            serialNumber).
+	 * @param b is a value of coin or banknote that have a value and currency (or
+	 *            serialNumber).
+	 * @return -1(negative integer) if a has order before b. 0(zero) if a and b have
+	 *         the same order. 1(positive integer) if a has order after b.
 	 */
 	public int compare(Valuable a, Valuable b) {
-        if (a.getCurrency().length() == (b.getCurrency()).length() ) {
-            if (a.getValue() < b.getValue()) return -1;
-            else if (a.getValue() > b.getValue()) return 1;
-            else return 0;
-        }
-        else if(a.getCurrency().length() < b.getCurrency().length()) return -1;
-		else return 1;
+		if (a.getCurrency().equals(b.getCurrency())) {
+			if (a.getValue() < b.getValue())
+				return -1;
+			else if (a.getValue() > b.getValue())
+				return 1;
+			else
+				return 0;
+		} else if (a.getCurrency().compareToIgnoreCase(b.getCurrency()) < 0) {
+			return -1;
+		} else
+			return 1;
 	}
 
 }
